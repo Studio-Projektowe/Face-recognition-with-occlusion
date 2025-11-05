b5e4a80a2abe7c06a0710ef6b53e69fb86e53fd2
Eliza <elizapetrycka@gmail.com>
2025-11-05 22:59:52 +0100

    Added buckets for large dataset CelebA

--- infrastructure/terraform/buckets.tf
+++ infrastructure/terraform/buckets.tf
@@ -3,6 +3,8 @@ resource "random_id" "suffix" {
   byte_length = 4
 }
 
+# Labeled Faces in the Wild
+
 resource "google_storage_bucket" "labeled-faces-in-the-wild-original" {
   name                     = "kaggle-labeled-faces-in-the-wild-original-${random_id.suffix.hex}"
   location                 = "US"
@@ -21,9 +23,46 @@ resource "google_storage_bucket" "labeled-faces-in-the-wild-original" {
   }
 }
 
-
 resource "google_storage_bucket" "labeled-faces-in-the-wild-occlusion" {
   name                     = "kaggle-labeled-faces-in-the-wild-occlusion-${random_id.suffix.hex}"
+  location                 = "US"
+
+  force_destroy            = false
+  public_access_prevention = "enforced"
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 120
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+
+# CelebA - CelebFaces
+
+resource "google_storage_bucket" "celeb-faces-original" {
+  name                     = "kaggle-celeb-faces-original-${random_id.suffix.hex}"
+  location                 = "US"
+
+  force_destroy            = false
+  public_access_prevention = "enforced"
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 120
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+
+resource "google_storage_bucket" "celeb-faces-occluded" {
+  name                     = "kaggle-celeb-faces-occluded-${random_id.suffix.hex}"
   location                 = "US"
 
   force_destroy            = false
--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,12 +1,6 @@
-
 variable "PROJECT_ID" {
-    description = "The ID of Google Cloud Project"
-    type        = string
-}
-
-variable "STATE_BUCKET" {
-    description = "The globally unique name for the GCS bucket to store Terraform state."
-    type        = string
+  description = "The ID of Google Cloud Project"
+  type        = string
 }
 
 variable "REGION" {
@@ -19,19 +13,4 @@ variable "ZONE" {
   description = "The default zone for GCP resources."
   type        = string
   default     = "northamerica-northeast1-b"
-}
-
-variable "LFITW_BUCKET_ORIGINAL" {
-  description = "The name of bucket containing 'Labeled Faces in the wild' original dataset"
-  type = string
-}
-  
-variable "LFITW_WITH_OCCLUSION" {
-  description = "The name of bucket containing 'Labeled Faces in the wild' modified dataset with occlusion"
-  type = string
-}
-
-variable "NOTEBOOK_NAME" {
-  description = "The name of workbench notebook"
-  type = string
 }
