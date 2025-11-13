b563a8720428ec5c589f9ee3c540ef62bcbe6024
Eliza <elizapetrycka@gmail.com>
2025-11-13 19:43:44 +0100

    Face recogintion model

---
+++ models/face_recognition/config.py
@@ -0,0 +1,14 @@
+import os
+
+# --- Konfiguracja Ścieżek Lokalnych ---
+BASE_FOLDER_LOCAL = "webface_112x112" 
+
+# --- Konfiguracja FAISS & Galerii ---
+FAISS_INDEX_FILE = "gallery.index"
+FAISS_MAPPING_FILE = "gallery_id_map.json"
+
+# --- Konfiguracja Ewaluacji ---
+RESULTS_CSV = "occlusion_results.csv"
+OCCLUSION_SIZE = 30 
+
+DETECTION_MODEL = "cnn"
